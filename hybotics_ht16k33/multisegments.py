0030f2660da86f8ef1621933d5483f0a511a99ec
Dale Weber <hybotics.wy@gmail.com>
2020-06-17 13:28:59 -0700

    Adding modified functions nulti_text(), multi_scroll(), and clear() test program. These work!

--- hybotics_ht16k33/multisegments.py
+++ hybotics_ht16k33/multisegments.py
@@ -1,19 +1,15 @@
 """
 Docstring
 """
-# from adafruit_bus_device.i2c_device import I2CDevice
-
+from time import sleep
+from micropython import const
 from hybotics_ht16k33.segments import Seg14x4
-
-# from adafruit_ht16k33.segments import Seg14x4
-
-# from micropython import const
 
 
 class MultiSeg14x4(Seg14x4):
     """Docstring"""
 
-    _DEFAULT_DISPLAY_BRIGHTNESS = 0.1
+    _DEFAULT_DISPLAY_BRIGHTNESS = const(0.1)
 
     def __init__(
         self,
@@ -23,25 +19,21 @@ class MultiSeg14x4(Seg14x4):
         brightness=_DEFAULT_DISPLAY_BRIGHTNESS,
         blink_rate=0,
     ):
-        # super().__init__(i2c, address, auto_write, brightness)
-        self._address = None
+        self._address = address
         self._auto_write = auto_write
         self._brightness = brightness
         self._blink_rate = blink_rate
-        self.devices = []
-        self._nr_disp = 0
+        self._devices = []
 
-        if isinstance(address, int):
+        if isinstance(self._address, int):
             print("DEBUG: (display.py) single display")
-            self.devices = Seg14x4(i2c, address, auto_write, brightness)
-            self._address = address
-        elif isinstance(address, list):
+            self._devices = Seg14x4(i2c, self._address, auto_write, brightness)
+        elif isinstance(self._address, list):
             print("DEBUG: (display.py) multi-display")
-            self._address = None
 
-            for index, addr in enumerate(address):
+            for index, addr in enumerate(self._address):
                 print(
-                    "DEBUG (display.py) display {0} at address = {1}".format(
+                    "DEBUG (display.py) display {0} at self._address = {1}".format(
                         index, hex(addr)
                     )
                 )
@@ -51,8 +43,8 @@ class MultiSeg14x4(Seg14x4):
                 self.devices[index]._brightness = brightness
                 self.devices[index]._blink_rate = blink_rate
 
-            self._nr_disp = len(self.devices)
-            self._nr_digits = self._nr_disp * 4
+            self._NUMBER_OF_DISPLAYS = const(len(self.devices))
+            self._NUMBER_OF_DIGITS = const(self._NUMBER_OF_DISPLAYS * 4)
             print("DEBUG (display.py) There are {0} displays".format(len(self.devices)))
 
     def fill(self, color):
@@ -71,7 +63,7 @@ class MultiSeg14x4(Seg14x4):
     def print(self, value, decimal=0):
         """Print the value to the display."""
         if isinstance(value, (str)):
-            self._text(value)
+            self._multi_text(value)
         elif isinstance(value, (int, float)):
             self._number(value, decimal)
         else:
@@ -79,46 +71,170 @@ class MultiSeg14x4(Seg14x4):
         if self._auto_write:
             self.show()
 
-    def _text(self, text):
-        """Display the specified text."""
-        for character in text:
-            self._push(character)
-
-    def _multitext(self, text):
-        """Display the specified text."""
+    def multi_text(self, text, show=True, scroll=False):
+        """Docstring"""
         length = len(text)
 
-        if length > self.nr_digits:
+        if length > self._NUMBER_OF_DIGITS:
+            if scroll:
+                pass
+            else:
+                raise ValueError(
+                    "Input overflow - '{0}' is too long for the display!".format(text)
+                )
+
+        if length > self._NUMBER_OF_DIGITS:
+            # Also could choose to scroll the text instead of raising an error
+            raise ValueError(
+                "The text, '{0}', is too long for the display!".format(text)
+            )
+
+        self.clear(self._devices)
+
+        for nr, _ in enumerate(self._address):
+            self._devices[nr].print(text[length - (nr + 1) * 4 : length - (nr * 4)])
+
+        self._devices[len(self._address) - 1].print(
+            text[0 : length - int(length / 4) * 4]
+        )
+
+        if show:
+            for nr, _ in enumerate(self._address):
+                self._devices[nr].show()
+
+    def multi_scroll(self, text, wait=1, show=True, loop=False):
+        """Scroll the specified text to the display, with optional looping."""
+
+        """
+        Initialize
+        """
+        length = len(text)
+
+        # This will probably not be needed for scrolling
+        if length > self._NUMBER_OF_DIGITS:
             raise ValueError(
                 "Input overflow - '{0}' is too long for the display!".format(text)
             )
 
         char_nr = 0
+        disp_index = 0
         disp_nr = 0
         disp_pos = 0
-        shift_pos = 0
+        digits_per_display = 4
+        shifting = False
 
-        while char_nr < length and disp_nr < self.nr_disp:
-            disp_nr = char_nr // 4
+        disp_shift = []
 
-            if disp_pos == 3:
-                # Must shift everything one character to the left
-                disp_nr += 1
-                self.devices[disp_nr].print(text[shift_pos])
-                shift_pos += 1
-                disp_pos = 0
+        for _, _ in enumerate(self._address):
+            disp_shift.append(0)
+
+        """
+        Starting up
+        """
+        print()
+
+        print("Starting up")
+        while char_nr < length and disp_index < self._NUMBER_OF_DISPLAYS:
+            print(
+                "DEBUG(1) disp_nr = {0}, char_nr = {1}, disp_pos = {2}, _NUMBER_OF_DISPLAYS = {3}, length = {4}".format(
+                    disp_nr, char_nr, disp_pos, self._NUMBER_OF_DISPLAYS, length
+                )
+            )
+
+            if not shifting:
+                print("Initializing displays for shifting")
+                print(
+                    "DEBUG(2) disp_nr = {0}, disp_pos = {1}, char = '{2}'".format(
+                        disp_nr, disp_pos, text[char_nr]
+                    )
+                )
+
+                self._devices[disp_nr].print(text[char_nr])
+
+                if disp_pos == 3:
+                    disp_pos = 0
+
+                    if disp_nr < self._NUMBER_OF_DISPLAYS:
+                        print(
+                            "DEBUG(3) Resetting display position and incrementing display"
+                        )
+
+                        disp_nr += 1
+                else:
+                    disp_pos += 1
+
                 char_nr += 1
-                self.devices[0].print(text[char_nr])
+                sleep(wait)
 
-                for d_nr in range(self.nr_disp, -1, -1):
-                    self.devices[d_nr].print(text[char_nr - disp_pos - 1])
-                    disp_pos += 1
-                    shift_pos += 1
-                    disp_pos += 1
+            nr_shifts = char_nr // digits_per_display
+            shifting = nr_shifts > 0
+            disp_nr = nr_shifts
 
-            self.devices[disp_nr].print(text[char_nr])
-            char_nr += 1
-            disp_pos += 1
+            """
+            Entering into the shifty state - everything has to be shifted to
+            the left now.
+            """
+            print()
+
+            while shifting and disp_nr > 0 and char_nr < length:
+                print("Inside the shifty state")
+
+                print(
+                    "DEBUG(4) disp_nr = {0}, char = '{1}', disp_shift = {2}, disp_pos = {3}".format(
+                        disp_nr, text[char_nr], disp_shift, disp_pos
+                    )
+                )
+
+                """
+                Do all the shifting
+                """
+                while disp_nr > 0 and char_nr < length:
+                    if disp_pos == 3:
+                        disp_pos = 0
+
+                    print(
+                        "Shifting for display {0}, disp_shift = {1}".format(
+                            disp_nr, disp_shift
+                        )
+                    )
+
+                    print(
+                        "    Shifting character '{0}' to the left on display {1}".format(
+                            text[disp_shift[disp_nr]], disp_nr
+                        )
+                    )
+
+                    self._devices[disp_nr].print(text[disp_shift[disp_nr]])
+                    disp_shift[disp_nr] += 1
+
+                    print(
+                        "DEBUG(5) disp_pos = {0}, char_nr = {1},  disp_nr = {2}".format(
+                            disp_pos, char_nr, disp_nr
+                        )
+                    )
+
+                    if disp_nr < 2:
+                        print(
+                            "    Adding new character '{0}' to display 0".format(
+                                text[char_nr]
+                            )
+                        )
+
+                        self._devices[0].print(text[char_nr])
+                        char_nr += 1
+                        disp_pos += 1
+
+                    disp_nr -= 1
+
+                    print()
+
+                    sleep(wait)
+
+        if show:
+            for nr, _ in enumerate(self._address):
+                self._devices[nr].show()
+
+        sleep(2)
 
     def _number(self, number, decimal=0):
         """
@@ -138,8 +254,8 @@ class MultiSeg14x4(Seg14x4):
         stnum = str(number)
         dot = stnum.find(".")
 
-        if len(stnum) > self.nr_digits + 1 or (
-            (len(stnum) > self.nr_digits) and (dot < 0)
+        if len(stnum) > self._NUMBER_OF_DIGITS + 1 or (
+            (len(stnum) > self._NUMBER_OF_DIGITS) and (dot < 0)
         ):
             raise ValueError(
                 "Input overflow - {0} is too long for the display!".format(number)
@@ -164,12 +280,12 @@ class MultiSeg14x4(Seg14x4):
         elif places > 0:
             txt = stnum[:places]
 
-        if len(txt) > self.nr_digits + 1:
+        if len(txt) > self._NUMBER_OF_DIGITS + 1:
             raise ValueError(
                 "Input overflow - {0} is too long for the display!".format(txt)
             )
 
-        self._multitext(txt)
+        self._multi_text(txt)
         self._auto_write = auto_write
 
         return txt
@@ -187,9 +303,9 @@ class MultiSeg14x4(Seg14x4):
     @property
     def nr_disp(self):
         """The number of displays"""
-        return self._nr_disp
+        return self._NUMBER_OF_DISPLAYS
 
     @property
     def nr_digits(self):
         """The number of displays"""
-        return self._nr_digits
+        return self._NUMBER_OF_DIGITS
